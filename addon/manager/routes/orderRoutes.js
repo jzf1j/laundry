65a7a8074d947438823cc0bb86aa6bc7e7096315
feng jiang <jzf111j@gmail.com>
2018-01-09 23:07:27 -0500

    email

--- addon/manager/routes/orderRoutes.js
+++ addon/manager/routes/orderRoutes.js
@@ -12,7 +12,7 @@ mOrderRouter.route('/').get(function (req, res) {
   Orders.find().populate('order_author_id').populate('order_products.buy_product')
   .sort({order_pickup_date: 1,order_pickup_time: 1}).exec(function(err, result_order) {
       if(err){return console.log(err);}
-      console.log(result_order[0].order_products[0].buy_product);
+      //console.log(result_order[0].order_products[0].buy_product);
         res.render('manager/orders/orders', {order: result_order});
       
     });
